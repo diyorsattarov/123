6befc80f0d46ebc8715dbbd8df48137173ab91c4
diyooor <diyorsattarov7@gmail.com>
2023-10-26 20:14:47 -0500

    Added: tick marks on the x and y axis added scaling when the red circle exceeds boundaries it gets properly scaled Bug: when the tick marks/x and y axis are scaled the positioning of the tick marks gets dense and distoreted, furthermore when the window is resized in the x and y direction the positioning of the oppisite axis doesn't get changed. e.g. when i increase window size in the y direction the x axis moves in the direction of the y axis changing and vice versa

--- tests/gui/test_gui.cpp
+++ tests/gui/test_gui.cpp
@@ -10,32 +10,81 @@ public:
 protected:
     void paintEvent(QPaintEvent* event) override {
         QPainter painter(this);
+        
+        // Clear the background with a black color
+        painter.fillRect(event->rect(), Qt::black);
+
         painter.setPen(Qt::white);
-        painter.drawLine(0, height() / 2, width(), height() / 2);
-        painter.drawLine(width() / 2, 0, width() / 2, height());
 
         // Calculate the position of the red circle after scaling
         int x = width() / 2 + static_cast<int>(xPos * scaleFactor);
         int y = height() / 2 - static_cast<int>(yPos * scaleFactor);
+
+        // Calculate the actual size of the scene based on the scaleFactor
+        int sceneWidth = width() / scaleFactor;
+        int sceneHeight = height() / scaleFactor;
+
+        // Calculate the fixed positions of the x and y axes
+        int xAxisY = height() / 2;
+        int yAxisX = width() / 2;
+
+        // Draw the x and y axes at fixed positions
+        painter.drawLine(0, xAxisY, sceneWidth, xAxisY);
+        painter.drawLine(yAxisX, 0, yAxisX, sceneHeight);
 
         // Draw the red circle at the origin
         painter.setPen(Qt::red);
         painter.setBrush(Qt::red);
         int radius = 5; // Adjust the radius as needed
         painter.drawEllipse(x - radius, y - radius, 2 * radius, 2 * radius);
+
+        // Draw ticks on the x and y axes
+        painter.setPen(Qt::white);
+        int tickLength = 5; // Adjust the tick length as needed
+
+        // X-axis ticks
+        for (int i = -sceneWidth / 2; i <= sceneWidth / 2; i += 50) { // Adjust the spacing between ticks
+            int tickX = width() / 2 + static_cast<int>(i * scaleFactor);
+            painter.drawLine(tickX, xAxisY - tickLength, tickX, xAxisY + tickLength);
+        }
+
+        // Y-axis ticks
+        for (int i = -sceneHeight / 2; i <= sceneHeight / 2; i += 50) { // Adjust the spacing between ticks
+            int tickY = height() / 2 - static_cast<int>(i * scaleFactor);
+            painter.drawLine(yAxisX - tickLength, tickY, yAxisX + tickLength, tickY);
+        }
     }
+
 
 public:
     void moveObject(double deltaX, double deltaY) {
-        // Move the object along the X and Y axes
-        xPos += deltaX;
-        yPos += deltaY;
+        // Calculate new positions after the movement
+        double newX = xPos + deltaX;
+        double newY = yPos + deltaY;
 
-        // Check if the object is out of bounds, and adjust the scaleFactor for zooming
-        if (xPos > width() / 2 || yPos > height() / 2) {
-            scaleFactor /= 2.0;
+        // Check if the new positions are out of bounds
+        if (qAbs(newX) > width() / (2 * scaleFactor) || qAbs(newY) > height() / (2 * scaleFactor)) {
+            // Calculate the scaling factor to keep the object within bounds
+            double scale = qMin((width() / (2 * qAbs(newX))), (height() / (2 * qAbs(newY))));
+
+            // Update the scale factor
+            scaleFactor *= scale;
+
+            // Update positions with the new scale
+            xPos = newX;
+            yPos = newY;
+        } else {
+            xPos = newX;
+            yPos = newY;
         }
 
+        update(); // Trigger repaint
+    }
+
+    void resetObject() {
+        xPos = 0;
+        yPos = 0;
+        scaleFactor = 1.0;
         update(); // Trigger repaint
     }
 
@@ -50,7 +99,7 @@ int main(int argc, char *argv[]) {
 
     CustomWidget window;
     window.setStyleSheet("background-color: black;");
-    window.resize(320, 240);
+    window.resize(640, 480);
     window.show();
     window.setWindowTitle(QApplication::translate("toplevel", "Top-level widget"));
 
@@ -74,6 +123,7 @@ int main(int argc, char *argv[]) {
         QString text = input.text().simplified();
         if (text.toLower() == "stop") {
             timer.stop();
+            window.resetObject();
         } else {
             // Parse the function and calculate deltaX and deltaY based on the function
             // Here, we assume a simple linear function "y = x" for demonstration purposes
@@ -85,7 +135,7 @@ int main(int argc, char *argv[]) {
 
     QObject::connect(&input, &QLineEdit::returnPressed, [&]() {
         // Start the timer when a new function is entered
-        timer.start(1000); // Adjust the timer interval as needed
+        timer.start(100); // Adjust the timer interval as needed
     });
 
     return app.exec();
